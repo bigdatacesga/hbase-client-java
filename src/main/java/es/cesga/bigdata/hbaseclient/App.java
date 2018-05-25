57520b013adfd2a91c651bc58fb2a1002b53511b
Javier Cacheiro <alcachi@gmail.com>
2018-05-25 10:13:54 +0200

    Fix indentation using always spaces

--- src/main/java/es/cesga/bigdata/hbaseclient/App.java
+++ src/main/java/es/cesga/bigdata/hbaseclient/App.java
@@ -15,28 +15,29 @@ import java.util.UUID;
 
 public class App {
 
-	private static final Logger logger = LoggerFactory.getLogger(App.class);
-	
+    private static final Logger logger = LoggerFactory.getLogger(App.class);
+
     public static void main(String[] args) throws IOException {
 
-		Configuration conf = HBaseConfiguration.create();
-		// Option 1 (recommended): Place hbase-site.xml on src/main/resources
-		// Option 2: Configure the hbase connection properties here:
+        Configuration conf = HBaseConfiguration.create();
+        // Option 1 (recommended): Place hbase-site.xml on src/main/resources
+        // Option 2: Configure the hbase connection properties here:
         // - Zookeeper addresses
-    	conf.set("hbase.zookeeper.quorum","10.112.13.19,10.112.13.18,10.112.13.17");
-    	// - Non-default znode parent (e.g. HDP with security disabled)
-        conf.set("zookeeper.znode.parent", "/hbase-unsecure");
+        //conf.set("hbase.zookeeper.quorum","10.112.13.19,10.112.13.18,10.112.13.17");
+        conf.set("hbase.zookeeper.quorum","10.112.14.8");
+        // - Non-default znode parent (e.g. HDP with security disabled)
+        //conf.set("zookeeper.znode.parent", "/hbase-unsecure");
 
         // Create a connection (you can use a try with resources block)
         Connection connection = ConnectionFactory.createConnection(conf);
 
-		// Creating a table with a random name
-		Admin admin = connection.getAdmin();
-		UUID uuid = UUID.randomUUID();
-		TableName tableName = TableName.valueOf(uuid.toString());
-		HTableDescriptor tableDescriptor = new HTableDescriptor(tableName);
-		HColumnDescriptor cf = new HColumnDescriptor(Bytes.toBytes("cf"));
-		tableDescriptor.addFamily(cf);
+        // Creating a table with a random name
+        Admin admin = connection.getAdmin();
+        UUID uuid = UUID.randomUUID();
+        TableName tableName = TableName.valueOf(uuid.toString());
+        HTableDescriptor tableDescriptor = new HTableDescriptor(tableName);
+        HColumnDescriptor cf = new HColumnDescriptor(Bytes.toBytes("cf"));
+        tableDescriptor.addFamily(cf);
         logger.info("Creating table {}", tableName);
         admin.createTable(tableDescriptor);
 
@@ -48,43 +49,51 @@ public class App {
         put.addColumn(Bytes.toBytes("cf"), Bytes.toBytes("name"), Bytes.toBytes("Javier"));
         table.put(put);
 
+        for (int i = 2; i < 10; i++) {
+            Put p = new Put(Bytes.toBytes("000" + i));
+            p.addColumn(Bytes.toBytes("cf"), Bytes.toBytes("name"), Bytes.toBytes("Javier-" + i));
+            table.put(p);
+        }
+
         logger.info("Reading a row");
-		Get get = new Get(Bytes.toBytes("0001"));
-		Result r = table.get(get);
-		byte[] value = r.getValue(Bytes.toBytes("cf"), Bytes.toBytes("name"));
-		logger.info("Result: Row: {} Value: {}", r, Bytes.toString(value));
+        Get get = new Get(Bytes.toBytes("0001"));
+        Result r = table.get(get);
+        byte[] value = r.getValue(Bytes.toBytes("cf"), Bytes.toBytes("name"));
+        logger.info("Result: Row: {} Value: {}", r, Bytes.toString(value));
 
         logger.info("Full scan");
-		Scan scan = new Scan();
-		ResultScanner scanner = table.getScanner(scan);
-		for (Result res : scanner) {
-			logger.info("Row: {}", res);
-		}
-		scanner.close();
+        Scan scan = new Scan();
+        ResultScanner scanner = table.getScanner(scan);
+        for (Result res : scanner) {
+            logger.info("Row: {}", res);
+        }
+        scanner.close();
 
-		logger.info("Restricted Scan");
-		Scan scan2 = new Scan();
-		scan2.addColumn(Bytes.toBytes("cf"), Bytes.toBytes("name"))
-				.setStartRow(Bytes.toBytes("0003"))
-				.setStopRow(Bytes.toBytes("0005"));
-		ResultScanner scanner2 = table.getScanner(scan2);
-		for (Result res2 : scanner2) {
-			System.out.println(res2);
-		}
-		scanner2.close();
+        logger.info("Restricted Scan");
+        Scan scan2 = new Scan();
+        scan2.addColumn(Bytes.toBytes("cf"), Bytes.toBytes("name"))
+                .setStartRow(Bytes.toBytes("0003"))
+                .setStopRow(Bytes.toBytes("0005"));
+        ResultScanner scanner2 = table.getScanner(scan2);
+        for (Result res2 : scanner2) {
+            System.out.println(res2);
+            byte[] value2 = res2.getValue(Bytes.toBytes("cf"), Bytes.toBytes("name"));
+            System.out.println("Value: " + Bytes.toString(value2));
+        }
+        scanner2.close();
 
         // Close the table
         table.close();
 
-		// Delete the table
+        // Delete the table
         logger.info("Disabling table {}", tableName);
-		admin.disableTable(tableName);
+        admin.disableTable(tableName);
         logger.info("Deleting table {}", tableName);
-		admin.deleteTable(tableName);
+        admin.deleteTable(tableName);
 
-		// Close the connection
-		connection.close();
+        // Close the connection
+        connection.close();
 
-    	System.exit(0);
+        System.exit(0);
     }
 }
